4d27350d18f71ba50d206f8bd5917986c74cc894
yuxin12345 <1406978596@qq.com>
2017-09-18 16:59:31 +0800

    exercise

--- function_exercise2.go
+++ function_exercise2.go
@@ -3,12 +3,7 @@ import "fmt"
 import "math"
 func main(){
 	fmt.Println(MySqrt(0.64))
-
 }
-func MySqrt(a float64)(float64,err error){
-	if a<0{
-		return fmt.Println("err")
-	}else{
-	return math.Sqrt(a),nil
-	}
+func MySqrt(a float64)(float64){
+	return math.Sqrt(a)
 }
